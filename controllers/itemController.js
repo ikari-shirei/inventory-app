d3e8cfa6c6c537c3cbe4493f77f8b5fab7b005e1
ikari-shirei <algrsyu@protonmail.com>
2022-03-09 22:41:58 +0300

    Add item create post

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -2,6 +2,8 @@ var Category = require('../models/category')
 var Item = require('../models/item')
 var async = require('async')
 
+const { body, validationResult } = require('express-validator')
+
 // Get categories list
 exports.item_list_get = function (req, res, next) {
   Item.find().exec(function (err, results) {
@@ -41,4 +43,60 @@ exports.item_create_get = function (req, res, next) {
 }
 
 //Get item creation page
-exports.item_create_post = function (req, res, next) {}
+exports.item_create_post = [
+  body('name', 'Name must not be empty.').trim().isLength({ min: 1 }).escape(),
+  body('Description').optional({ checkFalsy: true }).escape(),
+  body('model', 'model must not be empty.')
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body('year', 'year must not be empty.').trim().isLength({ min: 1 }).escape(),
+  body('category').escape(),
+  body('number_in_stock', 'number_in_stock must not be empty.')
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body('price', 'price must not be empty.')
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+
+  (req, res, next) => {
+    const errors = validationResult(req)
+
+    const item = new Item({
+      name: req.body.name,
+      description: req.body.description,
+      model: req.body.model,
+      year: req.body.year,
+      category: req.body.category,
+      number_in_stock: req.body.number_in_stock,
+      price: req.body.price,
+    })
+
+    if (!errors.isEmpty()) {
+      Category.find().exec(function (err, results) {
+        if (err) {
+          return next(err)
+        }
+
+        // Success
+        res.render('item_form', {
+          categories: results,
+          item: item,
+          errors: errors.array(),
+        })
+      })
+      return
+    } else {
+      item.save(function (err) {
+        if (err) {
+          return next(err)
+        }
+
+        // Succesful go to item page
+        res.redirect(item.url)
+      })
+    }
+  },
+]
